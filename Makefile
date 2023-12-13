84654215452527a5a7b3687d472b3e5bd71b91c3
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-12-13 15:17:09 +0400

    Basic project setup

---
+++ Makefile
@@ -0,0 +1,32 @@
+.DEFAULT_GOAL := build-run
+
+setup:
+	gradle wrapper --gradle-version 8.4
+
+clean:
+	./gradlew clean
+
+build:
+	./gradlew clean build
+
+install:
+	./gradlew clean installDist
+
+run-dist:
+	./build/install/app/bin/app
+
+run:
+	./gradlew run
+
+test:
+	./gradlew test
+
+report:
+	./gradlew jacocoTestReport
+
+lint:
+	./gradlew checkstyleMain checkstyleTest
+
+build-run: build run
+
+.PHONY: build
---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+	java
+	application
+	jacoco
+	checkstyle
+	id("org.springframework.boot") version "3.2.1-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_20
+}
+
+application {
+	mainClass = "hexlet.code.AppApplication"
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.postgresql:postgresql:42.5.4")
+	implementation("com.h2database:h2:2.2.220")
+	compileOnly ("org.projectlombok:lombok:1.18.20")
+	annotationProcessor ("org.projectlombok:lombok:1.18.20")
+
+	implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStandardStreams = true
+	}
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required = true
+	}
+}

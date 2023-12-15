39a496535ff93b4f0cbbc0200b0b4bfd0ea53047
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-12-15 16:37:20 +0400

    Add Spring Security

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -15,8 +15,12 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -27,18 +31,16 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @EntityListeners(AuditingEntityListener.class)
 @ToString(onlyExplicitlyIncluded = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class User implements BaseEntity {
+public class User implements UserDetails, BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @ToString.Include
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotBlank
     @ToString.Include
     private String firstName;
 
-    @NotBlank
     @ToString.Include
     private String lastName;
 
@@ -48,11 +50,46 @@ public class User implements BaseEntity {
     private String email;
 
     @NotBlank
-    private String password;
+    private String passwordDigest;
 
     @LastModifiedDate
     private LocalDate updatedAt;
 
     @CreatedDate
     private LocalDate createdAt;
+
+    @Override
+    public String getPassword() {
+        return passwordDigest;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }

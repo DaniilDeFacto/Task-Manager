3bb6bcdad95431db4e30f7bdc5a07f626bf15270
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-12-19 15:42:50 +0400

    Update DataInitializer.java

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,6 +1,8 @@
 package hexlet.code.component;
 
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
+import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
 import hexlet.code.service.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
@@ -9,6 +11,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
@@ -16,16 +20,43 @@ public class DataInitializer implements ApplicationRunner {
     private final UserRepository userRepository;
 
     @Autowired
+    private final TaskStatusRepository taskStatusRepository;
+
+    @Autowired
     private final CustomUserDetailsService userService;
 
     @Override
     public void run(ApplicationArguments args) {
         if (userRepository.findByEmail("hexlet@example.com").isEmpty()) {
-            var email = "hexlet@example.com";
-            var userData = new User();
-            userData.setEmail(email);
-            userData.setPasswordDigest("qwerty");
-            userService.createUser(userData);
+            userService.createUser(generateAdmin());
         }
+        for (var status : generateDefaultStatuses()) {
+            if (taskStatusRepository.findBySlag(status.getSlag()).isEmpty()) {
+                taskStatusRepository.save(status);
+            }
+        }
+    }
+
+    public static User generateAdmin() {
+        var userData = new User();
+        userData.setEmail("hexlet@example.com");
+        userData.setPasswordDigest("qwerty");
+        return userData;
+    }
+
+    public static List<TaskStatus> generateDefaultStatuses() {
+        var draftStatus = getTaskStatus("Draft", "draft");
+        var toReviewStatus = getTaskStatus("To review", "to_review");
+        var toBeFixedStatus = getTaskStatus("To be fixed", "to_be_fixed");
+        var toPublishStatus = getTaskStatus("To publish", "to_publish");
+        var publishedStatus = getTaskStatus("Published", "published");
+        return List.of(draftStatus, toReviewStatus, toBeFixedStatus, toPublishStatus, publishedStatus);
+    }
+
+    public static TaskStatus getTaskStatus(String name, String slug) {
+        var taskStatus = new TaskStatus();
+        taskStatus.setName(name);
+        taskStatus.setSlag(slug);
+        return taskStatus;
     }
 }

a8e71856443a2dd3be4e5a5bef66ce263f76bdc9
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-12-21 21:46:51 +0400

    Fix LabelDTO.java

--- src/main/java/hexlet/code/dto/LabelDTO.java
+++ src/main/java/hexlet/code/dto/LabelDTO.java
@@ -3,8 +3,12 @@ package hexlet.code.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Setter
 @Getter
 public class LabelDTO {
+    private Long id;
     private String name;
+    private LocalDate createdAt;
 }
